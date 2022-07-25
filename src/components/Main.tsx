415acc52afc5aaf075556256d849d6e83d485987
Elona Kajtazi <MY_NAME@example.com>
2022-07-25 21:37:49 +0200

    broke down the css!!

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,9 @@
+import "../styles/Main.css";
 import flaticonWelcomeImage from "../assets/images/flaticon-welcome-image.png";
 import backArrow from "../assets/icons/back-arrow.png";
 import downloadButton from "../assets/icons/download-button.png";
 import rateStarButton from "../assets/icons/rate-star-button.png";
 import rubbishButton from "../assets/icons/rubbish-bin-delete-button.png";
- 
 
 function Main() {
   return (
